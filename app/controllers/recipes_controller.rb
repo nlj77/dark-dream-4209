df58f6b8e4f0e11f40135e398dbebcf7e792f1fd
Nick Jones <nickjones9605@gmail.com>
2022-07-19 16:47:29 -0600

    adds .order(name: :desc) to index action for ingredients controller and recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController 
     def index
-        @recipes = Recipe.all
+        @recipes = Recipe.all.order(name: :desc)
     end
     def show 
         @recipe = Recipe.find(params[:id])
